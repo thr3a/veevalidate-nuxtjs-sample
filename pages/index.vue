e576ed03b89990b64ff958e1ed7965573a0dc9d3
thr3a <a@example.com>
2020-11-23 17:50:53 +0900

    fix: HTMLミスってたので修正

--- pages/index.vue
+++ pages/index.vue
@@ -2,44 +2,46 @@
   <div>
     <h1>vee-validate サンプルコード集</h1>
     <ul>
-      <nuxt-link to="/basic">
-        基本
-      </nuxt-link>
-    </ul>
-    <ul>
-      <nuxt-link to="/locale">
-        日本語化
-      </nuxt-link>
-    </ul>
-    <ul>
-      <nuxt-link to="/custom_validate">
-        カスタムバリデーション
-      </nuxt-link>
-    </ul>
-    <ul>
-      <nuxt-link to="/form_submit">
-        全てのバリデーションが通るまで送信ボタンを無効化する
-      </nuxt-link>
-    </ul>
-    <ul>
-      <nuxt-link to="/form_submit2">
-        フォーム送信ボタンクリック時にバリデーションを実行
-      </nuxt-link>
-    </ul>
-    <ul>
-      <nuxt-link to="/first_validate">
-        ページ表示時に最初からバリデーションを実行
-      </nuxt-link>
-    </ul>
-    <ul>
-      <nuxt-link to="/errors">
-        Javascript内でバリデーションを操作
-      </nuxt-link>
-    </ul>
-    <ul>
-      <nuxt-link to="/bootstrap">
-        Bootstrapで使ってみる
-      </nuxt-link>
+      <li>
+        <nuxt-link to="/basic">
+          基本
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/locale">
+          日本語化
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/custom_validate">
+          カスタムバリデーション
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/form_submit">
+          全てのバリデーションが通るまで送信ボタンを無効化する
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/form_submit2">
+          フォーム送信ボタンクリック時にバリデーションを実行
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/first_validate">
+          ページ表示時に最初からバリデーションを実行
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/errors">
+          Javascript内でバリデーションを操作
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/bootstrap">
+          Bootstrapで使ってみる
+        </nuxt-link>
+      </li>
     </ul>
   </div>
 </template>
